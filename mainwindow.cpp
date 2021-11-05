cab67cfc09bc0bd86000bff272c5e20a6be4a1df
Johny250353 <72787337+Johny250353@users.noreply.github.com>
2021-11-05 11:46:21 +0100

    poprawki

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,7 +4,7 @@
 #include <QtCore>
 #include <cstdlib>
 #include <dos.h>
-#include <QAbstractButton>
+#include <QAbstractButton> //to raczej nie jest potrzebne
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -69,20 +69,20 @@ void MainWindow::on_pushButton_D20_clicked()
 }
 
 
-void MainWindow::on_pushButton_D100_clicked()
+void MainWindow::on_pushButton_D100_clicked() //ostatni szybki rzut
 {
-    int d1 = 0;
-    d1 = (std::rand()%100+d1+1);
-    ui->textEdit_historia->append("Rzut D100:"+  QString::number(d1));
+    int d1 = 0; //zerowanie wartosci rzutu przed
+    d1 = (std::rand()%100+d1+1);    //pprzypisanie losowej wartosci do d1
+    ui->textEdit_historia->append("Rzut D100:"+  QString::number(d1));  //po przycisnieciu przyciksu dodaje do teskstu text rzut D100  wartosc którą przyjeło d1
 }
 
 
-void MainWindow::on_pushButton_rzut_clicked()
+void MainWindow::on_pushButton_rzut_clicked()   //jesli nacisniemy przycisk rzuc!                                            --POCZĄTEK RZUT MODYFIKOWANY--
 {
-int q = 0; //wartosc modyfikatora
-    if(ui->p0->isChecked())
+int q = 0; //wartosc modyfikatora wyzerowana na starcie                                                                      --USTALANIE MODYFIKATORA Q--
+    if(ui->p0->isChecked()) //sprawdza czy jest zaznaczony jesli tak to
     {
-        q = 0;
+        q = 0;      //przypisuje wartosc q
     }
 
     if(ui->p10->isChecked())
@@ -105,32 +105,32 @@ int q = 0; //wartosc modyfikatora
         q = -10;
     }
 
-       QString spin = ui->spinBox_iloscrzutuw->text();  //wychwytuje zmiane w spinboxie
+       QString spin = ui->spinBox_iloscrzutuw->text();  //wychwytuje zmiane w spinboxie                                     --SPIN BOX ILOSC RZUTÓW A--
        int a = spin.toUInt();                           //przypisuje wartosc spinboxa wartosci a
 
-       QString spin2 = ui->spinBox_iloscscianek->text();// robi to samo co partia wyzej ale z innym spinboxiem
+       QString spin2 = ui->spinBox_iloscscianek->text();// robi to samo co partia wyzej ale z innym spinboxiem              --SPIN BOX ZAKRES RZUTU B--
        int b = spin2.toUInt();
 
-       int d1 = 0;
-       while (a > 0)
+       int d1 = 0;  //zeruje wartosc d1
+       int dM = 0;
+       while (a > 0)    //powtarza wszystko poki wartosc a jest wieksza od zera
        {
+       dM = d1;
 
-       d1 = (std::rand()%b+d1 +1);
-       ui->textEdit_historia->append("Rzut:"+  QString::number(d1+q));
-       a--;
+       d1 = (std::rand()%b+d1 +1);  //przypisuje d1 losową wartość do zakresu z b
+
+       if (d1+q<=0)
+{
+           ui->textEdit_historia->append("Rzut z mod: " + QString::number(q) + " Rzut: 1" + " Suma= (1)");
+           a--; //odejmuej 1 z wartosci a
+ }
+       else {
+           ui->textEdit_historia->append("Rzut z mod: " + QString::number(q) + " Rzut: " + QString::number(d1 - dM)  + " Suma= (" + QString::number(d1+q) + ")");  //dodaje wynik d1 wyswietlajac go na lisice uwzgledniajac modyfikator q
+                 a--; //odejmuej 1 z wartosci a
        }
-    //ui->textEdit_historia->setText(QString::number(a));
+       }
 
-
-
-    //int d1 = (std::rand()%100+d1 -100);
-    //ui->textEdit_historia->setText("Rzut D100:"+  QString::number(d1));
-
-
-
-    //int i
-    //QString spin=ui->spinBox_iloscrzutuw->QString::number(i);
-}
+}                                                                                   //                                      --KONIEC RZUT MODYFIKOWANY--
 
 
 
